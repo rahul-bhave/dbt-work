071e3c94939431541798385346ce8dbdc0ee8fd7
rahulbhave <55383777+rahul-bhave@users.noreply.github.com>
2023-03-29 16:33:51 +0530

    Update test_example_failing.py

--- jaffle_shop/tests/functional/test_example_failing.py
+++ jaffle_shop/tests/functional/test_example_failing.py
@@ -55,7 +55,7 @@ class TestExample:
         assert len(results) == 8
         # test tests
         results = run_dbt(["test"], expect_pass = False) # expect failing test
-        assert len(results) == 26
+        assert len(results) == 25
         # validate that the results include one pass and one failure
         result_statuses = sorted(r.status for r in results)
         assert result_statuses == ["fail", "pass"]
